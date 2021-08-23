3c09ccd74d576dd4e0f260f938f23a8f8a5f676b
Dennis Mwaniki <damos5776@gmail.com>
2021-08-23 15:47:59 +0300

    index view

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,8 +13,8 @@ class RegForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(),Email()])
     username = StringField('Enter Your Username', validators=[Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    sumit = SubmitField('Sign Up')
+    password_confir = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
         if User.query.filter_by(email = data_field.data).first():
